42a23e66286061d0c807a3b134118991295b7c92
Lichal2108 <richard232108@gmail.com>
2025-05-13 17:54:00 -0500

    Creacion de clases principales

---
+++ src/app/component/registrar-persona/registrar-persona.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registrar-persona',
+  imports: [],
+  templateUrl: './registrar-persona.component.html',
+  styleUrl: './registrar-persona.component.css'
+})
+export class RegistrarPersonaComponent {
+
+}
