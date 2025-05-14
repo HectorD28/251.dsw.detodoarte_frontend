d565e88699100ef193637db647694aba8cb94e83
hector <hectorda28yo@gmail.com>
2025-05-14 15:36:47 -0500

    Solicitud de exposición básica

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
 import { RegistrarPersonaComponent } from './component/registrar-persona/registrar-persona.component';
 import { PerfilComponent } from './component/perfil/perfil.component'; // Asegúrate de importar el componente correctamente
+import { SolicitudExposicionComponent } from './component/solicitud-exposicion/solicitud-exposicion.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
     {path: 'registrar-persona',component:RegistrarPersonaComponent},
-    { path: 'perfil', component: PerfilComponent },
+    {path: 'perfil', component: PerfilComponent },
+    {path: 'solicitud-exposicion', component: SolicitudExposicionComponent}
 ];
 
--- src/app/component/solicitud-exposicion/solicitud-exposicion.component.ts
+++ src/app/component/solicitud-exposicion/solicitud-exposicion.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule} from '@angular/forms';
 import { SolicitudExposicionService } from '../../service/solicitud-exposicion.service';
 
 @Component({
   selector: 'app-solicitud-exposicion',
-  templateUrl: './solicitud-exposicion.component.html'
+  templateUrl: './solicitud-exposicion.component.html',
+  imports: [ReactiveFormsModule],
+  styleUrl: './solicitud-exposicion.component.css'
+
 })
 export class SolicitudExposicionComponent {
   solicitudForm: FormGroup;
@@ -28,5 +31,11 @@ export class SolicitudExposicionComponent {
           error: (err) => alert('Error al crear la solicitud')
         });
     }
+  }
+
+  onObraIdsChange(event: any) {
+    const value = event.target.value;
+    const ids = value.split(',').map((id: string) => Number(id.trim())).filter((id: number) => !isNaN(id));
+    this.solicitudForm.patchValue({ obraIds: ids });
   }
 }
